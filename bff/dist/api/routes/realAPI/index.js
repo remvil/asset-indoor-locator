92eb1e1222f1d424906fba3e8029beb134519551
remvil <rvildacci@gmail.com>
2024-07-06 16:28:27 +0200

    bff build

---
+++ bff/dist/api/routes/realAPI/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.realAPIRoutes = void 0;
+var assets_1 = require("./assets");
+var map_1 = require("./map");
+var paths_1 = require("./paths");
+exports.realAPIRoutes = { assetsRouter: assets_1.assetsRouter, mapRouter: map_1.mapRouter, pathsRouter: paths_1.pathsRouter };
